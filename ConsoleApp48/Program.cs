111e5b77b206e6d984242809e45168d1e5e5bc02
0xDanielFry <24DF4532@barton.ac.uk>
2025-07-09 09:43:29 +0100

    Fixed spellings

--- ConsoleApp48/Program.cs
+++ ConsoleApp48/Program.cs
@@ -13,26 +13,26 @@ namespace ConsoleApp48
                                              //                                                Settings                                                                        \\
                                              //                                           __|__________________________________________________________________________        \\
             double alpha = 1.1;              //    Prey's natural growth rate               |    Recommended:       1 - 2     Min:           1     Max:           5            \\
-            double beta = 0.1;               //    Preditor efficiency                      |    Recommended:  0.01 - 0.6     Min:        0.01     Max:           1            \\
-            double gamma = 0.4;              //    Preditor's death rate                    |    Recommended:   0.3 - 0.6     Min:         0.1     Max:           1            \\
+            double beta = 0.1;               //    Predator efficiency                      |    Recommended:  0.01 - 0.6     Min:        0.01     Max:           1            \\
+            double gamma = 0.4;              //    Predator's death rate                    |    Recommended:   0.3 - 0.6     Min:         0.1     Max:           1            \\
             double delta = 0.1;              //    How many prey are converted              |    Recommended:  0.05 - 0.4     Min:        0.01     Max:           1            \\
-                                             //    into preditor births.                    |                                                                                  \\
+                                             //    into predator births.                    |                                                                                  \\
                                              //                                             |                                                                                  \\
             double prey_population = 100;    //    Inital prey population.                  |    Recommended:    10 - 150     Min:           2     Max:       10000            \\
-            double preditor_population = 10; //    Inital preditor population.              |    Recommended:      1 - 15     Min:           2     Max:       10000            \\
+            double predator_population = 10; //    Inital predator population.              |    Recommended:      1 - 15     Min:           2     Max:       10000            \\
             double time_step = 0.1;          //    Increments in time.                      |    Recommended:  0.05 - 0.2     Min:        0.01     Max:           5            \\
             double total_time = 24;          //    Total time to run simulation.            |    Recommended:     5 - 100     Min:           1     Max:        1000            \\
                                              //                                             |                                                                                  \\
                                              //                                                                                                                                \\
 
             // Store the peaks and mins with a time
-            string peak_prey = $"{prey_population}, 0", peak_preditors = $"{preditor_population}, 0", min_prey = $"{prey_population}, 0", min_preditors = $"{preditor_population}, 0";
+            string peak_prey = $"{prey_population}, 0", peak_predators = $"{predator_population}, 0", min_prey = $"{prey_population}, 0", min_predators = $"{predator_population}, 0";
 
             Console.WriteLine("\n                                    Population");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"Initial Prey Population:            {prey_population}");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Initial Preditor Population:        {preditor_population}");
+            Console.WriteLine($"Initial Predator Population:        {predator_population}");
 
             Console.ForegroundColor = ConsoleColor.White;
 
@@ -40,35 +40,35 @@ namespace ConsoleApp48
             for (double time = 0; time < total_time; time += time_step)
             {
                 double prey_growth = alpha * prey_population;
-                double preditor_consumption = beta * prey_population * preditor_population;
-                double prey_change = prey_growth - preditor_consumption;
+                double predator_consumption = beta * prey_population * predator_population;
+                double prey_change = prey_growth - predator_consumption;
 
-                double preditor_growth = delta * preditor_consumption;
-                double preditor_death = gamma * preditor_population;
-                double preditor_change = preditor_growth - preditor_death;
+                double predator_growth = delta * predator_consumption;
+                double predator_death = gamma * predator_population;
+                double predator_change = predator_growth - predator_death;
 
                 prey_population = prey_population + (prey_change * time_step);
-                preditor_population = preditor_population + (preditor_change * time_step);
+                predator_population = predator_population + (predator_change * time_step);
 
                 // Checks for peak and min populations
-                if ( prey_population < double.Parse(min_prey.Split(',')[0]) ) // Mininum number of prey
+                if ( prey_population < double.Parse(min_prey.Split(',')[0]) ) // Minimum number of prey
                 {
                     min_prey = $"{prey_population}, {time}";
                 }
-                if ( preditor_population < double.Parse(min_preditors.Split(',')[0])) // Mininum number of preditors
+                if ( predator_population < double.Parse(min_predators.Split(',')[0])) // Minimum number of predators
                 {
-                    min_preditors = $"{preditor_population}, {time}";
+                    min_predators = $"{predator_population}, {time}";
                 }
                 if ( prey_population > double.Parse(peak_prey.Split(',')[0])) // Peak number of prey
                 {
                     peak_prey = $"{prey_population}, {time}";
                 }
-                if ( preditor_population > double.Parse(peak_preditors.Split(',')[0]) ) // Peak number of preditors
+                if ( predator_population > double.Parse(peak_predators.Split(',')[0]) ) // Peak number of predators
                 {
-                    peak_preditors = $"{preditor_population}, {time}";
+                    peak_predators = $"{predator_population}, {time}";
                 }
 
-                if ( prey_population <= 1 || preditor_population <= 1 ) // Break loop if population is too low
+                if ( prey_population <= 1 || predator_population <= 1 ) // Break loop if population is too low
                 {
                     break;
                 }
@@ -76,14 +76,14 @@ namespace ConsoleApp48
                 Console.WriteLine();
                 Console.WriteLine($"Time:                               {time}/{total_time}");
                 Console.WriteLine($"Prey Population:                    {prey_population}");
-                Console.WriteLine($"Preditor Population:                {preditor_population}");
+                Console.WriteLine($"Predator Population:                {predator_population}");
             }
 
             Console.WriteLine("\n                                    Population");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"Final Prey Population:              {prey_population}");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Final Preditor Population:          {preditor_population}");
+            Console.WriteLine($"Final Predator Population:          {predator_population}");
 
             Console.ForegroundColor = ConsoleColor.White;
 
@@ -91,11 +91,11 @@ namespace ConsoleApp48
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"Peak Prey:                          {peak_prey}");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Peak Preditors:                     {peak_preditors}");
+            Console.WriteLine($"Peak Predators:                     {peak_predators}");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine($"Mininum Prey:                       {min_prey}");
+            Console.WriteLine($"Minimum Prey:                       {min_prey}");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Mininum Preditors:                  {min_preditors}");
+            Console.WriteLine($"Minimum Predators:                  {min_predators}");
 
             Console.ReadKey();
 
